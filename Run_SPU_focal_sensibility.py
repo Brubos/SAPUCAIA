87882cd80af213a0e5db47f42ac63b16f8e6c2bf
Bruno Souza <bruno_ssts@hotmail.com>
2023-11-12 00:56:50 -0300

    Update all the code

--- Run_SPU_focal_sensibility.py
+++ Run_SPU_focal_sensibility.py
@@ -20,12 +20,8 @@ import matplotlib.pyplot as plt                                              # L
 import pandas as pd                                                          # Library for data manipulation and analysis
 
 #%% PARAMETERS:
-n_rays= 1000000                            # number of rays used in the simulation
-deg_f='Ry'                                 # the degree of freedom in Sirius frame;
-unit ='µrad'                               # µrad, mrad for rotations; [µm], [mm] for translations
-misalig_array=[10]#np.linspace(-10,10,3)        # misalignment array;
-
-position = 16970                           # Position of interest
+n_rays= 1000000                               # number of rays used in the simulation
+misalig_mirror=np.linspace(2.5,4.5,21)      # mirror misalignment array;
 
 # caustic parameters
 z0 = 0           # starting z
@@ -33,26 +29,9 @@ zf = 30000       # final z
 nz = 11          # number of points
 
 
-# Define a dictionary to map positions to devices
-position_to_device = {
-    0     : 'M1',
-    841   : 'DVF3',
-    13750 : 'DVF4',
-    16970 : 'SMP',
-    22000 : 'Hor_f',
-    25000 : 'Ver_f'    }
-
 # Set the default values for nbins_x and nbins_y
-nbins_x = 200
-nbins_y = 200
-
-# Check if the position is in the dictionary
-if position in position_to_device:
-    device = position_to_device[position]
-    
-    # Update nbins_x based on the device
-    if device == 'DVF3':
-        nbins_x = 90        # better parameters for this case
+nbins_x = 400
+nbins_y = 400
 
 
 #%% Lists to store data:
@@ -92,8 +71,8 @@ oe4 = Shadow.OE()
 #
 
 oe0.FDISTR = 3
-oe0.FILE_BOUND = b'/home/ABTLUS/bruno.souza/GITHUB/SAPUCAIA/SPU_optimize_source_66x66urad2.txt'  # CNPEM
-# oe0.FILE_BOUND = b'/home/bruno/GITHUB/SAPUCAIA/SPU_optimize_source_66x66urad2.txt'             # Home
+# oe0.FILE_BOUND = b'/home/ABTLUS/bruno.souza/GITHUB/SAPUCAIA/SPU_optimize_source_66x66urad2.txt'  # CNPEM
+oe0.FILE_BOUND = b'/home/bruno/GITHUB/SAPUCAIA/SPU_optimize_source_66x66urad2.txt'             # Home
 oe0.F_BOUND_SOUR = 2
 oe0.F_PHOT = 0
 oe0.HDIV1 = 0.0
@@ -128,8 +107,8 @@ oe1.T_REFLECTION = 180.0
 oe1.T_SOURCE = 20750.0
 
 oe2.DUMMY = 0.1
-oe2.FILE_REFL = b'/home/ABTLUS/bruno.souza/Oasys/Si111.dat'   # CNPEM
-# oe2.FILE_REFL = b'/home/bruno/Oasys/Si111.dat'              # HOME
+# oe2.FILE_REFL = b'/home/ABTLUS/bruno.souza/Oasys/Si111.dat'   # CNPEM
+oe2.FILE_REFL = b'/home/bruno/Oasys/Si111.dat'              # HOME
 oe2.FWRITE = 1
 oe2.F_CENTRAL = 1
 oe2.F_CRYSTAL = 1
@@ -142,8 +121,8 @@ oe2.T_SOURCE = 8250.0
 
 oe3.ALPHA = 180.0
 oe3.DUMMY = 0.1
-oe3.FILE_REFL = b'/home/ABTLUS/bruno.souza/Oasys/Si111.dat'  # CNPEM
-# oe3.FILE_REFL = b'/home/bruno/Oasys/Si111.dat'              # HOME
+# oe3.FILE_REFL = b'/home/ABTLUS/bruno.souza/Oasys/Si111.dat'  # CNPEM
+oe3.FILE_REFL = b'/home/bruno/Oasys/Si111.dat'              # HOME
 oe3.FWRITE = 1
 oe3.F_CENTRAL = 1
 oe3.F_CRYSTAL = 1
@@ -261,18 +240,10 @@ if iwrite:
 
 #%% RUNNING ONLY MIRROR FOR DIFFERENT MISALIGNMENT VALUES 
 
-for misalig in misalig_array:    
+for misalig in misalig_mirror:    
     
-    print("####################################################################")
-    
-    if deg_f in ['Rx', 'Ry', 'Rz']:
-        if unit == 'µrad': print('For variation of {} µrad'.format(misalig))
-        if unit == 'mrad': print('For variation of {} mrad'.format(misalig))
-    
-    if deg_f in ['Tx', 'Ty', 'Tz']:
-        if unit == 'mm': print('For variation of {} mm'.format(round(misalig,3))) # round the number
-        if unit == 'µm': print('For variation of {} µm'.format(misalig))
-        
+    print("####################################################################")  
+    print(misalig)
     print("####################################################################") 
     
     beam_copy = beam.duplicate()   # make a copy of the beam
@@ -283,10 +254,10 @@ for misalig in misalig_array:
     oe4.ALPHA = 90.0
     oe4.DUMMY = 0.1
     oe4.FHIT_C = 1 
-    oe4.FILE_REFL = b'/home/ABTLUS/bruno.souza/Oasys/Rh.dat'                                          # CNPEM
-    oe4.FILE_RIP = b'/home/ABTLUS/bruno.souza/GITHUB/SAPUCAIA/SPU_total_deformation_300mm_sh.dat'     # CNPEM
-    # oe4.FILE_REFL = b'/home/bruno/Oasys/Rh.dat'                                          # HOME
-    # oe4.FILE_RIP = b'/home/bruno/GITHUB/SAPUCAIA/SPU_total_deformation_300mm_sh.dat'     # HOME
+    # oe4.FILE_REFL = b'/home/ABTLUS/bruno.souza/Oasys/Rh.dat'                                          # CNPEM
+    # oe4.FILE_RIP = b'/home/ABTLUS/bruno.souza/GITHUB/SAPUCAIA/SPU_total_deformation_300mm_sh.dat'     # CNPEM
+    oe4.FILE_REFL = b'/home/bruno/Oasys/Rh.dat'                                          # HOME
+    oe4.FILE_RIP = b'/home/bruno/GITHUB/SAPUCAIA/SPU_total_deformation_300mm_sh.dat'     # HOME
     oe4.FMIRR = 3
     oe4.FWRITE = 1
     oe4.F_DEFAULT = 0
@@ -300,61 +271,12 @@ for misalig in misalig_array:
     oe4.RWIDX2 = 2.5
     oe4.SIMAG = 25000.0
     oe4.SSOUR = 31000.0
-    oe4.THETA = 89.7994647717
+    oe4.THETA = 90-np.degrees(misalig*1e-3)
     oe4.T_IMAGE = 0.0
-    oe4.T_INCIDENCE =  89.7994647717
-    oe4.T_REFLECTION = 89.7994647717
+    oe4.T_INCIDENCE =  90-np.degrees(misalig*1e-3)
+    oe4.T_REFLECTION = 90-np.degrees(misalig*1e-3)
     oe4.T_SOURCE = 0.0
     
-    
-    #%% Assigning misalignment values:
-    
-    # Rotation angle mapping to variables
-    rotation_mapping = {
-    "Rx": "Z_ROT",
-    "Ry": "X_ROT",
-    "Rz": "Y_ROT"      }
-     
-    rotation_factors = {
-    ('Rx', 'µrad'): -1 * np.rad2deg(misalig * 1e-6),
-    ('Ry', 'µrad'): -1 * np.rad2deg(misalig * 1e-6),
-    ('Rz', 'µrad'):  1 * np.rad2deg(misalig * 1e-6),                                    
-    ('Rx', 'mrad'): -1 * np.rad2deg(misalig * 1e-3),
-    ('Ry', 'mrad'): -1 * np.rad2deg(misalig * 1e-3),
-    ('Rz', 'mrad'):  1 * np.rad2deg(misalig * 1e-3)
-    }
-       
-    
-    # Translation mapping to variables
-    translation_mapping = {
-    "Tx": "OFFZ",
-    "Ty": "OFFX",
-    "Tz": "OFFY"          }
-    
-    translation_factors = {
-    ('Tx', 'mm'): -1 * misalig,
-    ('Ty', 'mm'): -1 * misalig,
-    ('Tz', 'mm'):  1 * misalig,
-    ('Tx', 'µm'): -1 * misalig * (1e-3),
-    ('Ty', 'µm'): -1 * round(misalig, 3) * 1e-3,
-    ('Tz', 'µm'):  1 * misalig * 1e-3             }
-       
-    key = (deg_f,unit)
-    
-    if deg_f in rotation_mapping:
-        rotation_variable = rotation_mapping[deg_f]
-        value = rotation_factors[key]
-        setattr(oe4, rotation_variable, value)
-       
-    if deg_f in translation_mapping:
-        translation_variable = translation_mapping[deg_f]
-        value = translation_factors[key]
-        setattr(oe4, translation_variable, value)
-  
-    print("####################################################################") 
-    print(oe4.Z_ROT)
-    print("####################################################################") 
-
     #
     # Run optical element 4
     #
@@ -368,87 +290,18 @@ for misalig in misalig_array:
         oe4.write("end.04")
         beam_copy.write("star.04")
    
-    # Choose the visualization element based on the position              
-    device = position_to_device.get(position, 'Unknown Device')
-    
-    #%% Read Shadow beam
+      
+   #%% Read Shadow beam
     beam2D = read_shadow_beam(beam_copy, x_column_index=3, y_column_index=1, nbins_x=nbins_x, nbins_y=nbins_y, nolost=1, ref=23, zeroPadding=2, gaussian_filter=0)
-         
-    #%% Adjustable the scale and plotting
-    
-    #%% Rotations
-    if deg_f == 'Rx': 
-        plot_range_x, plot_range_y = 1200, 1200      # the total range of the plot in [µm]    
-        zero_pad_x, zero_pad_y = 2, 2              # the zeros      
-        
-    if deg_f == 'Ry': 
-        plot_range_x, plot_range_y = 6000, 6000    # the total range of the plot in [µm] 
-        zero_pad_x, zero_pad_y = 8, 4              # the zeros      
-        
-    if deg_f == 'Rz': 
-        plot_range_x, plot_range_y = 10400, 10400  #  the total range of the plot in [µm]   
-        zero_pad_x, zero_pad_y = 25, 25            # the zeros
-        
-    #%% Translations    
-    if deg_f == 'Tx': 
-        plot_range_x, plot_range_y = 450, 450      # the total range of the plot in [µm]    
-        zero_pad_x, zero_pad_y = 2, 2              # the zeros      
-        
-    if deg_f == 'Ty': 
-        plot_range_x, plot_range_y = 9000, 9000    # the total range of the plot in [µm] 
-        zero_pad_x, zero_pad_y = 10, 40              # the zeros      
-        
-    if deg_f == 'Tz': 
-        plot_range_x, plot_range_y = 460, 460  # the total range of the plot in [µm]   
-        zero_pad_x, zero_pad_y = 5, 5            # the zeros
-            
- 
-    #%% Read Shadow beam
-    beam2D = read_shadow_beam(beam_copy, x_column_index=3, y_column_index=1, nbins_x=nbins_x, nbins_y=nbins_y, nolost=1, ref=23, zeroPadding=2, gaussian_filter=0)
-        
-    
-    #%% Adjustable the scale and plotting
-    
-    # Rotations
-    if deg_f == 'Rx': 
-        plot_range_x, plot_range_y = 2000, 2000      # the total range of the plot in [µm]    
-        zero_pad_x, zero_pad_y = 2, 2              # the zeros      
-        
-    if deg_f == 'Ry': 
-        plot_range_x, plot_range_y = 6000, 6000    # the total range of the plot in [µm] 
-        zero_pad_x, zero_pad_y = 8, 4              # the zeros      
-        
-    if deg_f == 'Rz': 
-        plot_range_x, plot_range_y = 17000, 17000  #  the total range of the plot in [µm]   
-        zero_pad_x, zero_pad_y = 14, 14            # the zeros
-        
-    # Translations    
-    if deg_f == 'Tx': 
-        plot_range_x, plot_range_y = 800, 800      # the total range of the plot in [µm]    
-        zero_pad_x, zero_pad_y = 2, 2              # the zeros      
-        
-    if deg_f == 'Ty': 
-        plot_range_x, plot_range_y = 4600, 4600    # the total range of the plot in [µm] 
-        zero_pad_x, zero_pad_y = 6, 6              # the zeros      
-        
-    if deg_f == 'Tz': 
-        plot_range_x, plot_range_y = 800, 800  # the total range of the plot in [µm]   
-        zero_pad_x, zero_pad_y = 5, 5            # the zeros
-        
-    #%% Filename
-    
-    # Rotations
-    if deg_f in ['Rx','Ry','Rz']: 
-        if unit =='µrad': filename = 'SPU '+ device + ' ' + deg_f +'=%.0fµrad'%(misalig)
-        if unit =='mrad': filename = 'SPU '+ device + ' ' + deg_f +'=%.0fmrad'%(misalig)
-        
-    # Translations
-    if deg_f in ['Tx','Ty','Tz']:
-        if unit =='mm': filename = "SPU "+ device + ' ' + deg_f +"=%.1fmm"%(misalig)
-        if unit =='µm': filename = 'SPU '+ device + ' ' + deg_f +'=%.0fµm'%(misalig)
         
     #%% Plot Beam
-    outputs = plot_beam(beam2D, show_plot=True,outfilename=filename+'.png',outfileext='png',cut=0,textA=1,textB=5,textC=2,fitType=3,cmap='viridis',plot_title=filename,
+    
+    zero_pad_x=0
+    zero_pad_y=0
+    plot_range_x=3000
+    plot_range_y=3000
+    
+    outputs = plot_beam(beam2D, show_plot=False,outfilename='',outfileext='png',cut=0,textA=1,textB=5,textC=2,fitType=3,cmap='viridis',plot_title='',
                         zero_pad_x=zero_pad_x, zero_pad_y=zero_pad_y,
                         x_range = plot_range_x, y_range = plot_range_y,
                         x_range_min= -(plot_range_x/2)/1000,
@@ -472,13 +325,10 @@ for misalig in misalig_array:
     
     # Ru caustic
     run_shadow_caustic(filename=Caustic, beam=beam_copy, zStart=z0, zFin=zf, nz=nz, zOffset=0, colh=3, colv=1, colref=23, nbinsh=200, nbinsv=200,
-                        xrange=[-2,2], 
-                        yrange=[-2,2])
-                       
-                       # xrange=goodRange[0:2], 
-                       # yrange=goodRange[2:4])
+                        xrange=goodRange[0:2], 
+                        yrange=goodRange[2:4])
 
-    histo_h, histo_v, caustic_dict = read_caustic(Caustic,plot=False,plot2D=False,cmap='viridis',figprefix='')
+    histo_h, histo_v, caustic_dict = read_caustic(Caustic,plot=False,plot2D=False,cmap='viridis',figprefix='',print_minimum=False)
     
     #%% Store the outputs   
     data['misalignment'].append(misalig)
@@ -486,156 +336,19 @@ for misalig in misalig_array:
     data['fwhm_v_sli'].append(outputs['fwhm_z'])
     data['mean_pos_h'].append(outputs['mean_x'])
     data['mean_pos_v'].append(outputs['mean_z'])
-    data['z_fwhm_min_h'].append(caustic_dict['z_fwhm_min_h']+31000)   # sum 31000 due position of the mirror
-    data['z_fwhm_min_v'].append(caustic_dict['z_fwhm_min_v']+31000)   # sum 31000 due position of the mirror
+    data['z_fwhm_min_h'].append(caustic_dict['z_fwhm_min_h']*(1e-3)+31)   # sum 31000 due position of the mirror
+    data['z_fwhm_min_v'].append(caustic_dict['z_fwhm_min_v']*(1e-3)+31)   # sum 31000 due position of the mirror
     data['z_rms_min_h'].append(caustic_dict['center_rms_h'])
     data['z_rms_min_v'].append(caustic_dict['center_rms_v'])   
          
-#%% Plot the focus sensibility
-#%% Rotations
-
-############################ Rx - Yall ############################
-if(deg_f == 'Rx'): 
-   
-    # Create a figure with two subplots and increase the vertical gap between them
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'hspace': 0.4})
-
-    # Plot the horizontal focus position on the first subplot
-    ax1.plot(data['misalignment'], data['z_rms_min_h'], 'b', label='Horizontal Focus Position')
-    ax1.set_ylabel('Horizontal focus position')
-
-    # Plot the vertical focus position on the second subplot
-    ax2.plot(data['misalignment'], data['z_rms_min_v'], 'r', label='Vertical Focus Position')
-    ax2.set_xlabel(deg_f + ' [' + unit + '] - Yall')
-    ax2.set_ylabel('Vertical focus position')
-
-    # Enable minor ticks for both subplots
-    ax1.minorticks_on()
-    ax2.minorticks_on()
-
-    # Show the grid for both subplots
-    ax1.grid(True)
-    ax2.grid(True)
-
-    # Set a title for the entire figure
-    plt.suptitle('Focal Sensitivity')
-
-    plt.tight_layout()  # Adjust subplot spacing
-    plt.show()
-    
-############################ Ry - Pitch ############################    
-if(deg_f == 'Ry'):
-    plt.plot(data['misalignment'], data['z_rms_min_h'],  '.', color='black', label='data')
-    plt.xlabel(deg_f+' ['+unit+'] - Pitch') 
-    plt.grid(True)
-    plt.ylabel('Horizontal focus position')
-    plt.title('Focal Sensitivity')
-
-############################ Rz - Roll ############################    
-if(deg_f == 'Rz'):
-    plt.plot(data['misalignment'], data['z_rms_min_v'],   '.', color='black', label='data')
-    plt.xlabel(deg_f+' ['+unit+'] - Roll') 
-    plt.grid(True)
-    plt.ylabel('Vertical focus position')
-    plt.title('Focal Sensitivity')
-
-#%% Translations
-
-############################### Tx ###############################
-if(deg_f == 'Tx'): 
-   
-    # Create a figure with two subplots and increase the vertical gap between them
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'hspace': 0.4})
-
-    # Plot the horizontal focus position on the first subplot
-    ax1.plot(data['misalignment'], data['z_rms_min_h'],  'b', label='Horizontal Focus Position')
-    ax1.set_ylabel('Horizontal focus position')
-
-    # Plot the vertical focus position on the second subplot
-    ax2.plot(data['misalignment'], data['z_rms_min_v'],  'r', label='Vertical Focus Position')
-    ax2.set_xlabel(deg_f + ' [' + unit + '] - Yall')
-    ax2.set_ylabel('Vertical focus position')
-
-    # Enable minor ticks for both subplots
-    ax1.minorticks_on()
-    ax2.minorticks_on()
-
-    # Show the grid for both subplots
-    ax1.grid(True)
-    ax2.grid(True)
-
-    # Set a title for the entire figure
-    plt.suptitle('Focal Sensitivity')
-
-    plt.tight_layout()  # Adjust subplot spacing
-    plt.show()
-
-
-############################### Ty ###############################
-if(deg_f == 'Ty'): 
-   
-   # Create a figure with two subplots and increase the vertical gap between them
-   fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'hspace': 0.4})
-
-   # Plot the horizontal focus position on the first subplot
-   ax1.plot(data['misalignment'], data['z_rms_min_h'],  'b', label='Horizontal Focus Position')
-   ax1.set_ylabel('Horizontal focus position')
-
-   # Plot the vertical focus position on the second subplot
-   ax2.plot(data['misalignment'], data['z_rms_min_v'],  'r', label='Vertical Focus Position')
-   ax2.set_xlabel(deg_f + ' [' + unit + '] - Yall')
-   ax2.set_ylabel('Vertical focus position')
-
-   # Enable minor ticks for both subplots
-   ax1.minorticks_on()
-   ax2.minorticks_on()
-
-   # Show the grid for both subplots
-   ax1.grid(True)
-   ax2.grid(True)
-
-   # Set a title for the entire figure
-   plt.suptitle('Focal Sensitivity')
-
-   plt.tight_layout()  # Adjust subplot spacing
-   plt.show()
-    
-############################### Tz ###############################
-if(deg_f == 'Tz'): 
-    
-   # Create a figure with two subplots and increase the vertical gap between them
-   fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'hspace': 0.4})
-
-   # Plot the horizontal focus position on the first subplot
-   ax1.plot(data['misalignment'], data['z_rms_min_h'],  'b', label='Horizontal Focus Position')
-   ax1.set_ylabel('Horizontal focus position')
-
-   # Plot the vertical focus position on the second subplot
-   ax2.plot(data['misalignment'], data['z_rms_min_v'],  'r', label='Vertical Focus Position')
-   ax2.set_xlabel(deg_f + ' [' + unit + '] - Yall')
-   ax2.set_ylabel('Vertical focus position')
-
-   # Enable minor ticks for both subplots
-   ax1.minorticks_on()
-   ax2.minorticks_on()
-
-   # Show the grid for both subplots
-   ax1.grid(True)
-   ax2.grid(True)
-
-   # Set a title for the entire figure
-   plt.suptitle('Focal Sensitivity')
-
-   plt.tight_layout()  # Adjust subplot spacing
-   plt.show()
 #%% Save the data
 
 # Original header list
-header_list = [deg_f+' Misalignment '+'['+unit+']', 
-               'z_fwhm_min_h',
-               'z_fwhm_min_v',
-               'z_rms_min_h []',
-               'z_rms_min_v []',]
+header_list = [' Misalignment [mrad]', 
+               'z_fwhm_min_h [m]',
+               'z_fwhm_min_v [m]']
+               # 'z_rms_min_h []',
+               # 'z_rms_min_v []',]
 
 # Custom formatting function to center-align header
 def format_center_align_header(value):
@@ -649,12 +362,12 @@ data2 = pd.DataFrame({
     formatted_headers[0]: data['misalignment'],
     formatted_headers[1]: data['z_fwhm_min_h'],
     formatted_headers[2]: data['z_fwhm_min_v'],
-    formatted_headers[3]: data['z_rms_min_h'],  
-    formatted_headers[4]: data['z_rms_min_v']
+    # formatted_headers[3]: data['z_rms_min_h'],  
+    # formatted_headers[4]: data['z_rms_min_v']
 })
 
 # Specify the file name
-file_name = filename
+file_name = 'teste'
 
 # Custom formatting function to center-align data
 def format_center_align_data(value):
